d7b996a7f84d99cf3efdeb07ce1a54817278bf22
Dev <rastogi.db1@gmail.com>
2025-12-27 10:05:29 +0530

    update sizing

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,16 @@ import { carData } from "../data/carData";
 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto p-6 grid md:grid-cols-2 gap-6">
+    <main className="max-w-7xl mx-auto p-6 grid md:grid-cols-3 gap-6">
       
       {/* Left */}
-      <div className="">
+      <div className="md:col-span-2">
         <ImageCarousel images={carData.images} />
         <View360Button />
       </div>
 
       {/* Right */}
-      <div className="space-y-6">
+      <div className="md:col-span-1 space-y-6">
         <CarOverview car={carData} />
         <PriceCalculator basePrice={carData.price} />
       </div>
